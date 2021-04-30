a75daaa970e34ab347492b15b04bd753890ae3ba
rpitman <pitmanr@icloud.com>
2021-04-30 12:07:08 -0400

    Initial Commit

---
+++ PathsManuallyCreated/PathsManuallyCreatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathsManuallyCreatedApp.swift
+//  PathsManuallyCreated
+//
+//  Created by ronnie on 4/30/21.
+//
+
+import SwiftUI
+
+@main
+struct PathsManuallyCreatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
